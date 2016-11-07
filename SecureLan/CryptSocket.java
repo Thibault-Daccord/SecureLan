050efe7ad782a7d3e15dacc947c61903d872a041
Thibault-Daccord <thibault.daccord@gmail.com>
2016-11-07 19:12:08 +0100

    Update CryptSocket.java

--- SecureLan/CryptSocket.java
+++ SecureLan/CryptSocket.java
@@ -21,6 +21,11 @@ import SecureLan.cryptage.AESCrypt;
 import SecureLan.cryptage.RSACrypt;
 
 
+/**
+ * @author Thibault DACCORD
+ *
+ */
+
 public class CryptSocket{
 	ArrayList<Socket> sockets;
 	HashMap<Socket,byte[] > SocketAes = new HashMap<Socket,byte[]>();//socket,cle aes
@@ -34,13 +39,12 @@ public class CryptSocket{
 	public CryptSocket(Socket socket) {
 		sockets = new ArrayList<Socket>();
 		sockets.add(socket);
-		// TODO Auto-generated constructor stub
 	}
 	public CryptSocket(ArrayList<Socket> socketArray) {
 		sockets = socketArray;
 
-		// TODO Auto-generated constructor stub
 	}
+	
 	public CryptSocket(ArrayList<Socket> socketArray,InetAddress ip) {
 		sockets = new ArrayList<Socket>();
 
@@ -49,8 +53,9 @@ public class CryptSocket{
 				sockets.add(s);
 			}
 		}
-		// TODO Auto-generated constructor stub
 	}	
+	
+	
 	
 	public CryptSocket(ArrayList<Socket> socketArray,String ip) {
 		for(Socket s : socketArray){
@@ -61,65 +66,12 @@ public class CryptSocket{
 		}	
 	}
 	
-	/*/
-	private void encrypt() throws Exception{
-		DataInputStream in;
-		byte[] buf = new byte[genererReponseCleActivation().length];
-		for(Socket s :sockets){
-		     in = new DataInputStream(s.getInputStream());
-			 in.read(buf);
-			 if(!equals(buf,genererReponseCleActivation())){
-				 makeLink(s);
-			 }
-			 else{
-				 acceptLink(s);
-			 }
-		}	
-		
-	}
-	/*/
-	
-	 public void makeLink() throws Exception{
-		 for(Socket s : sockets){
-			 makeLink1(s);
-		 }
-	 }
-	
-	 void makeLink1(Socket s) throws Exception{
-		 
-		DataOutputStream out;
-		rsa= new RSACrypt();
-		rsa.generateKey();
-		PublicKey cleRsaPublic = rsa.getPublicKey();
-		PrivateKey cleRsaPrive = rsa.getPrivateKey();
-		byte[] q =cleRsaPublic.getEncoded();
-		String rep= genererQuestionCleActivation()+"#";
-		for(byte b :q){
-			rep+=","+b;
-		}		
-			 out = new DataOutputStream(s.getOutputStream());
-	          out.writeUTF(rep);
-		DataInputStream in;
-		     in = new DataInputStream(s.getInputStream());
-		     String questEtcleAesCrypte= in.readUTF();
-		     String repActivationRecu=questEtcleAesCrypte.split("#")[0];
-		     if(repActivationRecu.equals(this.genererReponseCleActivation())){
-			     String cleAesCrypte=questEtcleAesCrypte.split("#")[1];
-			     cleAesCrypte=cleAesCrypte.substring(1,cleAesCrypte.length());
-			     byte[] bcleAesCrypte=StringKeyToByte(cleAesCrypte);
-			     cleAes=rsa.decrypt(bcleAesCrypte);
 
-				  SocketAes.put(s,cleAes);
-				 
-				out.writeUTF("ok");
-		     }
-		     else{
-		    	 System.out.println("bad rep "+repActivationRecu);
-		     }
-		
-	}
-	
-	 
+	 /**
+	 * @throws Exception
+	 * permet de cree une connexion chiffree pour les sockets
+	 */
+		 
 	 public void acceptLink() throws Exception{
 		 for(Socket s : sockets){
 			 acceptLink(s);
@@ -159,7 +111,14 @@ public class CryptSocket{
 		rsaPublicKey=StringKeyToByte(sRsa);
 			EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(rsaPublicKey);
 			PublicKey pk = keyFactory.generatePublic(publicKeySpec);
+
+			
+			//System.out.println("la cle aes non crypte est "+scleAes);
 			cleAesCrypte = rsa.encrypt(cleAes,pk);
+			System.out.println("la cle aes crypte est ");
+			for(byte b : cleAesCrypte){
+				System.out.print(b);
+			}
 			DataOutputStream out;
 			out = new DataOutputStream(s.getOutputStream());
 			out.writeUTF(genererReponseCleActivation()+"#"+tabToString(cleAesCrypte));
@@ -176,7 +135,75 @@ public class CryptSocket{
 		
 		
 	}
+	
+	 
+	 /**
+	 * @throws Exception
+	 * accepte les connexions chiffrees
+	 */
+	public void acceptLink() throws Exception{
+		 for(Socket s : sockets){
+			 acceptLink(s);
+		 }
+	 
+	 }
+
+	 
+	 /**
+	 * @param s
+	 * @throws Exception
+	 * accepte les connexions chiffrees pour un socket
+	 */
+	private  void acceptLink(Socket s) throws Exception{
+			aes = new AESCrypt();
+			byte[] cleAesCrypte;
+			byte[] cleAes;
+			
+			cleAes = aes.generateKey();
+
+			byte[] rsaPublicKey;
+			String algorithm = "RSA"; // or RSA, DH, etc.
+			KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			boolean bon =false;
+			String rep="";
+			String sRsa="";
+			while(!bon){
+				rep = in.readUTF();
+				String[] q=rep.split("#,");
+				if(q.length>1){
+					String qq=q[0];
+					sRsa=q[1];
+					
+					if(qq.equals(genererQuestionCleActivation()));
+						bon=true;
+				}
+			}
+			
+			rsaPublicKey=StringKeyToByte(sRsa);
+				EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(rsaPublicKey);
+				PublicKey pk = keyFactory.generatePublic(publicKeySpec);
+				cleAesCrypte = rsa.encrypt(cleAes,pk);
+				DataOutputStream out;
+				out = new DataOutputStream(s.getOutputStream());
+				out.writeUTF(genererReponseCleActivation()+"#"+tabToString(cleAesCrypte));
+				 bon =false;
+				rep = in.readUTF();
+				if(rep.equals("ok")){
+					SocketAes.put(s, cleAes);
+				}
+				else{
+					System.out.println("echec de la connexion");
+				}
+			
+			
+		}
 		
+	/**
+	 * @param s
+	 * @return
+	 * transforme la cle sous la forme ",-1,5,..."
+	 */
 	private byte[] StringKeyToByte(String s){
 		String[] tab = s.split(",");
 		int cpt=0;
@@ -191,56 +218,33 @@ public class CryptSocket{
 	
 	
 	
-	private byte[] concat(byte[] a,byte[] b){
-		byte[] tab = new byte[a.length+b.length];
-		int cpt=0;
-		for(cpt=0;cpt<a.length;cpt++){
-			tab[cpt] = a[cpt];
-		}
-		for(int i=cpt;i<b.length;i++){
-			cpt++;
-			tab[cpt]= b[i];
-		}
-		return tab;
-		
-		
-	}
 	
-	
+	/**
+	 * @return
+	 * question pour demander l'activation distant d un socket crypte
+	 */
 	private String genererQuestionCleActivation(){
 		String s = "@Questcrypt42SecureLan2";
 		return s;	
 	}
 	
+	/**
+	 * @return
+	 * 	reponse pour la demande d activation distant d un socket crypte
+
+	 */
 	private String genererReponseCleActivation(){
 		String s = "@Repcrypt42SecureLan2";
 		return s;	
 	}	
 	
-	private boolean equals(byte[] a,byte b[]){
-		int lg = a.length;
-		if(lg!=b.length)
-			return false;
-		for(int i =0;i<lg;i++){
-			if(a[i]!=b[i]){
-				return false;
-			}
-		}
-		return true;
-		
-	}
+
 	
 	
-	private byte[] subByte(byte[] tab ,int a,int b){
-		byte[] retour = new byte[b-a];
-		for(int i=0;i<retour.length;i++){
-			retour[i] = tab[a];
-			a++;
-		}
-		return retour;
-		
-	}
-	
+	/**
+	 * @throws IOException
+	 * ferme les sockets
+	 */
 	public void fermer() throws IOException{
 		for(Socket s: sockets){
 			s.close();
@@ -253,6 +257,11 @@ public class CryptSocket{
 	
 	
 	
+	/**
+	 * @param tab
+	 * @throws IOException
+	 * ecrit dans les socket un tabeau de byte
+	 */
 	public void write(byte[] tab) throws IOException{
 		DataOutputStream out;
 		aes  = new AESCrypt();
@@ -265,6 +274,11 @@ public class CryptSocket{
 		}
 	}
 
+	/**
+	 * @return
+	 * @throws Exception
+	 * lis dans un socket un tableau de byte
+	 */
 	public byte[] readFully() throws Exception{
 		if(sockets.size()>1){
 			throw new Exception("il y a plus d un socket, utiliser la fonction readAllFully ");
@@ -282,6 +296,11 @@ public class CryptSocket{
 		throw new Exception("pas de socket");
 	}
 	
+	/**
+	 * @return
+	 * @throws IOException
+	 * lis dans tous les sockets un tableau de byte
+	 */
 	public ArrayList<byte[]> readAllFully() throws IOException{
 		DataInputStream in;
 		ArrayList<byte[]> retour = new ArrayList<byte[]>();
@@ -297,9 +316,20 @@ public class CryptSocket{
 		return retour;
 	}
 	
+	/**
+	 * @param msg
+	 * @throws IOException
+	 * ecrit dans les socket un string
+	 */
 	public void writeUTF(String msg) throws IOException{
 		write(msg.getBytes());
 	}
+	/**
+	 * @param msg
+	 * @return
+	 * @throws Exception
+	 *  lis dans un socket un string
+	 */
 	public String readUTF() throws Exception{
 		return new String(readFully());
 	}
@@ -308,17 +338,14 @@ public class CryptSocket{
 	
 	
 	
-	public  int available(Socket socket) throws IOException{
-		DataInputStream in;
-		in = new DataInputStream(socket.getInputStream());
-		return in.available();
-	}	
 	
 	
 	
-	
-	
-	
+	/**
+	 * @param tab
+	 * @return
+	 * transforme un tableau de byte en un string lisible
+	 */
 	public String tabToString(byte[] tab){
 		String retour="";
 		for(int i=0;i<tab.length;i++)
